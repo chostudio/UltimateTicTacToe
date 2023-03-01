4bb9e6b66731dba52fad6844bd33cf3afaf75950
DeAndreAgleham <aglehamd26@maryknollschool.org>
2023-02-28 18:02:04 -1000

    Button CLasses and some Id's
    Changed Button Classes and added Id's to Top left Tic Tac Toe. Prob gonna be the last pull request for a while to prevent myself from doing too much ._.

---
+++ app.js
@@ -0,0 +1,15 @@
+//Top Row
+let tr = document.getElementsByClassName("topRight");
+let tm = document.getElementsByClassName("topMid");
+let tl = document.getElementsByClassName("topLeft")
+
+//Middle Row
+let mr = document.getElementsByClassName("midRight");
+let mm = document.getElementsByClassName("midMid");
+let ml = document.getElementsByClassName("midLeft")
+
+//Bottom Row
+let br = document.getElementsByClassName("bottomRight");
+let bm = document.getElementsByClassName("bottomMid");
+let bl = document.getElementsByClassName("bottomLeft");
+
