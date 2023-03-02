d508aa467068facf331c42b67c1b8ee9ab78d47c
Cake <krupitskya@gmail.com>
2023-03-01 16:31:45 -1000

    Migrate lol
    Migrated to TypeScript + Webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  entry: './src/index.ts',
+  devServer: {
+    port: 8080,
+    open: true,
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.css'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
