487e4a3655b370d46f3853a385de71e85d07a61f
Cake <krupitskya@gmail.com>
2023-04-04 15:08:08 -1000

    Update (NOT WORKING)
    ok

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,6 @@
-import './style.css';
+import './style.css'
+import { Board, Grid, Cell} from './board'
 
-console.log("Hello world")
+const tds = document.querySelectorAll("table.grid") as NodeListOf<HTMLTableElement>;
+let board = new Board(tds)
 
-const tds: NodeListOf<Element> = document.querySelectorAll("table.innerBoard td");
-const cells: HTMLElement[][] = []
-
-let node = tds[0].parentElement?.parentElement;
-let cellNum = 0;
-let boardNum = 0;
-let board: HTMLElement[] = [];
-
-for (let cell of tds) {
-    if (cell.parentElement?.parentElement != node) {
-        node = cell.parentElement?.parentElement
-        cellNum = 0;
-        cells.push(board);
-        board = [];
-        boardNum++
-    }
-
-    const button = document.createElement('button');
-    button.innerText = `${boardNum} - ${cellNum}`;
-    button.classList.add("cell");
-    button.setAttribute("cellNum", cellNum.toString())
-    button.setAttribute("boardNum", boardNum.toString())
-    cell.appendChild(button);
-    board.push(button);
-    
-    button.addEventListener("click", (event: Event) => {
-        let target = event.target as HTMLElement;
-        console.log()
-    });
-    
-    
-    cellNum++
-}
-
-console.log(cells);
