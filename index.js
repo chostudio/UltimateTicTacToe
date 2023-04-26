1ad2a3b0385b14399fffe814b59d1fc045f629bb
DeAndreAgleham <aglehamd26@maryknollschool.org>
2023-04-25 16:01:58 -1000

    psh
    dsripton

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 let arr = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8]
+    [1, 0, 2],
+    [1, 2, 0],
+    [1, 0, 0]
 ]
 
+/*
+0 = None
+1 = X
+2 = O
+*/
+
+arr[1][1]++
+
 console.log(arr[1][1])
+
+let darr = [0, 0, 0]
+
+
+
+function allSame(toFind, array){
+    for(it of array){
+        if(it == toFind){
+            console.log("Ohian")
+        }
+    }
+}
+
+let arr1 = [1, 1, 1]
+let arr2 = [1, 2, 1]
+
+console.log(allSame(1, arr1))
+console.log(allSame(1, arr2))
